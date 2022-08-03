4610b9ccc513c2d4353187fe9f8b5788956e6e8b
Stefan Hagen <stefan@hagen.link>
2022-08-03 22:14:32 +0200

    Bumped impl version for initial release

--- xoxo/__init__.py
+++ xoxo/__init__.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG_NAME = '.xoxo.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.1+parent.7635e35c'
-# [[[end]]] (checksum: 336a0105003ee28994a568480df2fbd9)
+__version__ = '2022.8.3+parent.c13710db'
+# [[[end]]] (checksum: c1bde48c5a9bc2ff1483ef50805c75c4)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
