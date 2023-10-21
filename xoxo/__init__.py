197d49cd49d1ff59f5c30730f4656f3fc701bc5a
Stefan Hagen <stefan@hagen.link>
2023-10-21 14:42:33 +0200

    Bumped impl version for release 2023.10.21

--- xoxo/__init__.py
+++ xoxo/__init__.py
@@ -17,8 +17,8 @@ DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.4+parent.c7f09ef7'
-# [[[end]]] (checksum: 18b2041736060202db7f90641d05489e)
+__version__ = '2023.10.21+parent.c817d87e'
+# [[[end]]] (checksum: c2eff6e104a1048731a33399a3f20b50)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
